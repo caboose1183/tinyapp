05b89e37f01e403f408408e03986b564b98b7dfd
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-13 17:45:03 +0000

    added a route to show html code

--- express_server.js
+++ express_server.js
@@ -7,6 +7,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -14,6 +16,12 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
