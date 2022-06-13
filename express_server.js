2c3dbb10f6571476cf6bdecda3cf8e2c9293cb5e
Sidney Pang <sidneypang@hotmail.co.uk>
2022-06-13 23:11:54 +0000

    added recirects for when shortURL is created to the shortURL page, added redirect to longURL to actual page

--- express_server.js
+++ express_server.js
@@ -5,7 +5,7 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -37,15 +37,25 @@ app.get("/urls/new", (req, res) => {
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+
+  res.redirect(longURL);
 });
 
 
 
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+
+  urlDatabase[shortURL] = req.body.longURL;
+
+  res.redirect(302, `/urls/${shortURL}`);
 });
 
 
@@ -53,11 +63,11 @@ app.post("/urls", (req, res) => {
 function generateRandomString() {
   let codeList = ['a', 'b', 'c', 'd', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-  let shortURL = [];
+  let shortURL = '';
 
   for (let i = 0; i < 6; i++) {
-    let randomNum = Math.round ((Math.random () * 36));
-    shortURL.push (codeList[randomNum]);
+    let randomNum = Math.round((Math.random() * 36));
+    shortURL += codeList[randomNum];
   };
 
   return shortURL;
